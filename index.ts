fee28ef3b72609ce842aeb593a6ad7141a33585d
mappywin <mappydesuu@gmail.com>
2024-12-08 01:06:10 +0300

    final lab 2

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 //Вариант 2
 
+//1.1
+
 const findMinValue = (arr: number[]): number => {
     let minValue = arr[0];
 
@@ -17,6 +19,8 @@ const arr1: number[] = [32,21,24,6,522,124,67,4,523]
 const minValue = findMinValue(arr1);
 console.log(minValue);
 
+
+//1.2
 
 const findCountZero = (arr: number[][]): number => {
     let count = 0;
@@ -42,6 +46,7 @@ const arr2: number[][] = [
 const count = findCountZero(arr2);
 console.log(count);
 
+//2
 
 const strConcat = (tuple: [string, string, string]): string => {
     return tuple[0]+tuple[1]+tuple[2];
@@ -51,3 +56,75 @@ const tuple: [string, string, string] = ["Hello, ", "How are you? ", "What is yo
 
 const concatStr = strConcat(tuple);
 console.log(concatStr);
+
+//3
+
+enum TypesOfPrinters {
+    dotMatrix = "DotMatrix",
+    inkjet= "Inkjet",
+    laser = "Laser",
+    sublimation = "Sublimation",
+    threeD = "3D"
+};
+
+const printer: TypesOfPrinters = TypesOfPrinters.laser;
+console.log(printer);
+
+//4
+
+class Pet {
+    name: string = 'Some pet'
+    age: number = -1
+    speak() {
+        return "No speak. I am fish!";
+    }
+}
+
+class Dog extends Pet {
+    label = "AngryHunter";
+    age = 8;
+    speak() {
+        return "Yaw-Gaw!";
+    }
+}
+
+class Cat extends Pet {
+    name = 'Barsik';
+    age = 2;
+    speak() {
+        return "Miyau!";
+    }
+}
+
+function printPetInfo<T extends Pet>(pet: T) {
+    console.log(`Type: ${pet.constructor.name}`);
+    console.log(`Name: ${pet.name}`);
+    console.log(`Age: ${pet.age}`);
+    console.log(`Speak: ${pet.speak()}`);
+}
+
+const myDog = new Dog();
+const myCat = new Cat();
+
+console.log("-----------Dog-----------");
+printPetInfo(myDog);
+console.log("-----------Cat-----------");
+printPetInfo(myCat);
+
+//5
+
+type Printers = {
+    type: TypesOfPrinters;
+    model: string;
+    isColor: boolean;
+    price: number;
+}
+
+const myPrinter: Printers = {
+    type: TypesOfPrinters.laser,
+    model: "HP LaserJet M110we",
+    isColor: false,
+    price: 16199
+}
+
+console.log(JSON.stringify(myPrinter,  null, 2));
