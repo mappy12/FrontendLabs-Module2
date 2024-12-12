5df2ee66898dd97a9bc0d882e89948ec8df3d51a
mappy-mac <mappydesuu@gmail.com>
2024-12-12 22:19:45 +0400

    added Owner class

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ enum DocType {
     birthCertificate = "birthCertificate" 
 }
 
-interface Owner {
+interface IOwner {
     surname: string;
     name: string;
     patronymic: string;
@@ -17,7 +17,7 @@ interface Owner {
     print(): void;
 }
 
-interface Vehicle {
+interface IVehicle {
     brand: string;
     model: string;
     releaseYear: number;
@@ -25,5 +25,36 @@ interface Vehicle {
     regNumber: number;
     owner: Owner;
     print(): void;
+}
+
+
+class Owner implements IOwner {
+    private surname: string;
+    private name: string;
+    private patronymic: string;
+    private dateOfBirth: number;
+    private docType: DocType;
+    private docSeries: number;
+    private docNum: number;
+
+    constructor(surname: string, name: string, patronymic: string, dateOfBirth: number,
+        docType: DocType, docSeries: number, docNum: number) {
+            this.surname = surname;
+            this.name = name;
+            this.patronymic = patronymic;
+            this.dateOfBirth = dateOfBirth;
+            this.docType = docType;
+            this.docSeries = docSeries;
+            this.docNum = docNum;
+        }
+
+    public print(): void {
+        console.log(`Фамилия: ${this.surname}`);
+        console.log(`Имя ${this.name}`);
+        console.log(`Отчество: ${this.patronymic}`);
+        console.log(`Дата рождения: ${this.dateOfBirth}`);
+        console.log(`Тип документа: ${this.docType}`);
+        console.log(`Номер документа: ${this.docNum}`);
+    }
 }
 
