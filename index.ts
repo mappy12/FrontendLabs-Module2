d401ecab6364aaa70d98b85d8335fb50966a8235
mappy-mac <mappydesuu@gmail.com>
2024-12-28 20:43:21 +0400

    final 3 lab

--- index.ts
+++ index.ts
@@ -113,7 +113,6 @@ class Vehicle implements IVehicle {
         console.log(`Год выпуска: ${this._releaseYear}`);
         console.log(`VIN: ${this._VIN}`);
         console.log(`Регистрационный номер: ${this._regNumber}`);
-        console.log(`Владелец: ${this._owner}`);
     }
 
     constructor(brand: string, model: string, releaseYear: number, VIN: string,
@@ -153,5 +152,180 @@ class Vehicle implements IVehicle {
 }
 
 interface ICar extends IVehicle {
+    bodyType: BodyType;
+    carClass: CarClass;
+}
 
+class Car implements ICar {
+    private _brand: string;
+    private _model: string;
+    private _releaseYear: number;
+    private _VIN: string;
+    private _regNumber: number;
+    private _owner: Owner;
+
+    private _bodyType: BodyType;
+    private _carClass: CarClass;
+
+    constructor(brand: string, model: string, releaseYear: number, VIN: string,
+        regNumber: number, owner: Owner, bodyType: BodyType, carClass: CarClass) {
+           this._brand = brand;
+           this._model = model;
+           this._releaseYear = releaseYear;
+           this._VIN = VIN;
+           this._regNumber = regNumber;
+           this._owner = owner;
+
+           this._bodyType = bodyType;
+           this._carClass = carClass;
+    }
+
+    public print(): void {
+        console.log(`Брэнд: ${this._brand}`);
+        console.log(`Модель: ${this._model}`);
+        console.log(`Год выпуска: ${this._releaseYear}`);
+        console.log(`VIN: ${this._VIN}`);
+        console.log(`Регистрационный номер: ${this._regNumber}`);
+        console.log(`Тип кузова: ${this._bodyType}`);
+        console.log(`Класс автомобиля: ${this._carClass}`)
+    }
+    
+    get brand() {
+        return this._brand;
+    }
+
+    get model() {
+        return this._model;
+    }
+
+    get releaseYear() {
+        return this._releaseYear;
+    }
+
+    get VIN() {
+        return this._VIN;
+    }
+
+    get regNumber() {
+        return this._regNumber;
+    }
+
+    get owner() {
+        return this._owner;
+    }
+
+    get bodyType(): BodyType {
+        return this.bodyType;
+    }
+
+    get carClass(): CarClass {
+        return this._carClass;
+    }
+}
+
+interface IMotorbike extends IVehicle {
+    frameType: string;
+    isSport: boolean;
+}
+
+class Motorbike implements IMotorbike {
+    private _brand: string;
+    private _model: string;
+    private _releaseYear: number;
+    private _VIN: string;
+    private _regNumber: number;
+    private _owner: Owner;
+
+    private _frameType: string;
+    private _isSport: boolean;
+
+    constructor(brand: string, model: string, releaseYear: number, VIN: string,
+        regNumber: number, owner: Owner, frameType: string, isSport: boolean) {
+           this._brand = brand;
+           this._model = model;
+           this._releaseYear = releaseYear;
+           this._VIN = VIN;
+           this._regNumber = regNumber;
+           this._owner = owner;
+
+           this._frameType = frameType;
+           this._isSport = isSport;
+    }
+
+    public print(): void {
+        console.log(`Брэнд: ${this._brand}`);
+        console.log(`Модель: ${this._model}`);
+        console.log(`Год выпуска: ${this._releaseYear}`);
+        console.log(`VIN: ${this._VIN}`);
+        console.log(`Регистрационный номер: ${this._regNumber}`);
+
+        console.log(`Тип рамы: ${this._frameType}`);
+        console.log(`Подходит ли для спорта: ${this._isSport}`);
+    }
+
+    get brand() {
+        return this._brand;
+    }
+
+    get model() {
+        return this._model;
+    }
+
+    get releaseYear() {
+        return this._releaseYear;
+    }
+
+    get VIN() {
+        return this._VIN;
+    }
+
+    get regNumber() {
+        return this._regNumber;
+    }
+
+    get owner() {
+        return this._owner;
+    }
+
+    get frameType() {
+        return this._frameType;
+    }
+
+    get isSport(){
+        return this._isSport;
+    }
+}
+
+interface IVehicleStorage<T extends IVehicle> {
+    creationDate: Date;
+    vehicles: T[];
+    getAllVehicle(): T[];
+
+    addVehicle(vehicle: T): void;
+}
+
+class VehicleStorage<T extends Vehicle> implements IVehicleStorage<T> {
+    private _creationDate: Date;
+    private _vehicles: T[];
+
+    constructor() {
+        this._creationDate = new Date();
+        this._vehicles = [];
+    }
+
+    getAllVehicle(): T[] {
+        return this._vehicles;
+    }
+
+    addVehicle(vehicle: T): void {
+        this._vehicles.push(vehicle);
+    }
+
+    get creationDate() {
+        return this._creationDate;
+    }
+
+    get vehicles() {
+        return this._vehicles;
+    }
 }
