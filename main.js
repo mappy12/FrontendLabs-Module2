2411a02d32ba4eb5b3b3141eb825065a67281397
mappywin <mappydesuu@gmail.com>
2025-01-01 23:34:08 +0300

    backup

--- main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const me = new Transport.Owner("Zhestkov", "Andrey", "Alekseevich", "30.03.2006", Transport.DocType.passport, 1234, 344444);
-const myCar = new Transport.Car("Toyota", "Camry", 2024, "1HGCM82633A123456", 123456, me, Transport.BodyType.Sedan, Transport.CarClass.Luxury);
-myCar.print();
